016a72920eec4c317e794cf7176e9a31ff6183c6
Malik Abdul Salam <75979219+MalikAbdulSalam@users.noreply.github.com>
2026-09-24 14:49:26 +0500

    Add files via upload

---
+++ 25-calculator.py
@@ -0,0 +1,28 @@
+#if condition:
+#    do something hers
+#    do something hers
+#    do something hers
+#    do something hers
+#this is outside if
+
+
+a = input("Please enter 1st number  :   ")
+b = input("Please enter 2nd number  :   ")
+c = input("please enter operator    :   ")
+
+# convert a and b into integer
+a = int(a)
+b  = int(b)
+
+if c=="+":
+    print(a+b)
+
+if c=="-":
+    print(a-b)
+
+
+if c=="*":
+    print(a*b)
+
+if c=="/":
+    print(a/b)
